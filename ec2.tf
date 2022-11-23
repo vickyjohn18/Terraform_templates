db218c8ebe2322fb889ef46e2a3fed9f01a46406
vickyjohn18 <104903054+vickyjohn18@users.noreply.github.com>
2022-11-23 11:04:55 +0530

    Add files via upload
    AWS Ec2 instance creation with user data

---
+++ ec2.tf
@@ -0,0 +1,45 @@
+provider "aws" {
+    access_key = "AKIA42UKK54XRHEUEJ6H"
+    secret_key = "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"
+    region = "ap-south-1"
+   
+  
+}
+resource "aws_instance" "web" {
+  ami         = "ami-074dc0a6f6c764218"
+  instance_type = "t2.micro"
+  availability_zone = "ap-south-1a"
+  vpc_security_group_ids = [aws_security_group.sg.id]
+    user_data = <<-EOF
+  #!/bin/bash
+sudo su
+yum install httpd -y
+service httpd start
+service httpd enable
+  EOF
+  tags = {
+    Name = "app"
+  }
+}
+resource "aws_security_group" "sg" {
+    ingress {
+        description = "ssh port allow"
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = [ "0.0.0.0/0" ]
+    }
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = [ "0.0.0.0/0" ]
+    }
+    egress {
+      from_port = 0
+      to_port = 0
+      protocol = "all"
+      cidr_blocks = [ "0.0.0.0/0" ]
+    }
+  
+}
